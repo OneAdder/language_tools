8ae25cfdafd4ddbdbcdbb5d1ad89a4c53604794c
OneAdder <mikivo@list.ru>
2020-12-23 23:06:01 +0300

    забыл

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ import os
 import pathlib
 
 PYTHON = os.environ.get('WEB_DEVELOPMENT_PROJECT_PYTHON') or '/usr/bin/python3'
-ROOT = pathlib.Path(__file__).parent.resolve()
+ROOT = str(pathlib.Path(__file__).parent.resolve())
