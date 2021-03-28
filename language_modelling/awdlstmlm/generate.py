ee7edde6cd337333e7d92a159ab63aea2c09342d
OneAdder <mikivo@list.ru>
2021-03-28 21:40:26 +0300

    рефакторинг

--- language_modelling/awdlstmlm/generate.py
+++ language_modelling/awdlstmlm/generate.py
@@ -6,84 +6,105 @@
 ###############################################################################
 
 import argparse
+from typing import Tuple, Union
+from pathlib import Path
 
 import torch
 
 from language_modelling.awdlstmlm import data
 
-parser = argparse.ArgumentParser(description='PyTorch PTB Language Model')
+SEED = 1111
+TEMPERATURE = 1.0
 
-# Model parameters.
-parser.add_argument('--data', type=str, default='./data/penn',
-                    help='location of the data corpus')
-parser.add_argument('--model', type=str, default='LSTM',
-                    help='type of recurrent net (LSTM, QRNN)')
-parser.add_argument('--checkpoint', type=str, default='./model.pt',
-                    help='model checkpoint to use')
-parser.add_argument('--outf', type=str, default='generated.txt',
-                    help='output file for generated text')
-parser.add_argument('--words', type=int, default='1000',
-                    help='number of words to generate')
-parser.add_argument('--seed', type=int, default=1111,
-                    help='random seed')
-parser.add_argument('--cuda', action='store_true',
-                    help='use CUDA')
-parser.add_argument('--temperature', type=float, default=1.0,
-                    help='temperature - higher will increase diversity')
-parser.add_argument('--log-interval', type=int, default=100,
-                    help='reporting interval')
-parser.add_argument('--input', type=str, default='')
-args = parser.parse_args()
 
-# Set the random seed manually for reproducibility.
-torch.manual_seed(args.seed)
-if torch.cuda.is_available():
-    if args.cuda:
-        torch.cuda.manual_seed(args.seed)
+class Generator:
+    _model = None
+    _model_path = None
+    _corpus = None
 
-with open(args.checkpoint, 'rb') as f:
-    model, _, _ = torch.load(f)
-model.eval()
+    def __init__(self,
+                 model_path: Union[str, Path],
+                 corpus_path: Union[str, Path],
+                 seed: int = SEED,
+                 cuda: bool = True,
+                 temperature: float = TEMPERATURE):
+        if self._model is None or model_path != self._model_path:
+            torch.cuda.manual_seed(seed) if cuda else torch.manual_seed(seed)
+            self._model_path = model_path
+            self._cuda = cuda
+            self._model = self._load_model()
+            self._corpus, self._ntokens = self._load_corpus(corpus_path)
+            self._temperature = temperature
 
-if args.cuda:
-    model.cuda()
-else:
-    model.cpu()
+    def _load_model(self) -> torch.nn.Module:
+        with open(self._model_path, 'rb') as f:
+            model, _, _ = torch.load(f)
+        model.eval()
+        model.cuda() if self._cuda else model.cpu()
+        return model
 
-corpus = data.Corpus(args.data, args.cuda)
-ntokens = len(corpus.dictionary)
+    def _load_corpus(self, corpus_path) -> Tuple[data.Corpus, int]:
+        corpus = data.Corpus(corpus_path, self._cuda)
+        ntokens = len(corpus.dictionary)
+        return corpus, ntokens
 
-user_input_tokens = args.input.split(",")
-user_input = []
-for token in user_input_tokens:
-    idx = corpus.dictionary.word2idx.get(token)
-    if idx is None:
-        continue
-    user_input.append(idx)
-user_input = torch.tensor([user_input])
+    def predict_nis(self, segmented_data, top_k=3):
+        candidates = []
 
-if args.cuda:
-    user_input.data = user_input.data.cuda()
-hidden = model.init_hidden(user_input.size()[1])
+        with torch.no_grad():
+            input_idxs_tensor = torch.tensor([[0]]).to('cuda' if self._cuda else 'cpu')
+            hidden = self._model.init_hidden(input_idxs_tensor.size()[1])
 
-success = 0
-error = 0
-wpa = 0
+            for i in range(len(segmented_data)):
+                input_token = segmented_data[i]
+                idx = self._corpus.dictionary.word2idx.get(input_token)
+                input_idxs_tensor.data.fill_(idx)
+                output, hidden = self._model(input_idxs_tensor, hidden)
+                if i != len(segmented_data) - 1:
+                    continue
+                word_weights = self._model.decoder(output).squeeze().data.exp()
+                _, predictions = torch.topk(word_weights, top_k)
+                for new_idx in predictions:
+                    the_idx = new_idx.item()
+                    candidate = self._corpus.dictionary.idx2word[the_idx]
+                    candidates.append(candidate)
+        return candidates
 
-result = []
+    def generate(self, input_str: str, output_len: int) -> str:
+        user_input_tokens = input_str.split(",")
+        result = self.predict_nis(user_input_tokens, output_len)
+        return ','.join(result)
 
-for i in range(args.words):
-    output, hidden = model(user_input, hidden)
-    word_weights = model.decoder(output).squeeze().data.div(args.temperature).exp().cpu()
-    word_idx = torch.multinomial(word_weights, 1)[0]
-    user_input.data.fill_(word_idx[0])
-    try:
-        word = corpus.dictionary.idx2word[word_idx[0]]
-    except IndexError:
-        break
-    if word == '<eos>':
-        break
-    result.append(word)
 
-with open(args.outf, 'w') as f:
-    f.write(','.join(result))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch PTB Langmodeluage Model')
+
+    # Model parameters.
+    parser.add_argument('--data', type=str, default='./data/penn',
+                        help='location of the data corpus')
+    parser.add_argument('--model', type=str, default='LSTM',
+                        help='type of recurrent net (LSTM, QRNN)')
+    parser.add_argument('--checkpoint', type=str, default='./model.pt',
+                        help='model checkpoint to use')
+    parser.add_argument('--outf', type=str, default='generated.txt',
+                        help='output file for generated text')
+    parser.add_argument('--words', type=int, default='1000',
+                        help='number of words to generate')
+    parser.add_argument('--seed', type=int, default=SEED,
+                        help='random seed')
+    parser.add_argument('--cuda', action='store_true',
+                        help='use CUDA')
+    parser.add_argument('--temperature', type=float, default=TEMPERATURE,
+                        help='temperature - higher will increase diversity')
+    parser.add_argument('--log-interval', type=int, default=100,
+                        help='reporting interval')
+    parser.add_argument('--input', type=str, default='')
+    args = parser.parse_args()
+    generator = Generator(model_path=args.checkpoint,
+                          corpus_path=args.data,
+                          seed=args.seed,
+                          cuda=args.cuda,
+                          temperature=args.temperature)
+    result = generator.generate(args.input, int(args.words))
+    with open(args.outf, 'w') as f:
+        f.write(result)
